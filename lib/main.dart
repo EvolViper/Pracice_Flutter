5ff0bd70b250461d7d2303c34307a34e707e0851
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-10 13:51:06 +0700

    Goi Class

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,25 @@ import 'package:english_words/english_words.dart';
 void main() {
   runApp(const MyApp());
 }
+class RandomWords extends StatefulWidget{
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
         debugShowCheckedModeBanner: false,
@@ -19,7 +31,8 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           //child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+          //child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
