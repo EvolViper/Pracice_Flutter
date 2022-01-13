0aca5ffa12420886c466041af3b050b7567b05f0
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-13 15:44:15 +0700

    Learning Flexible, Expanded, SizedBox, Spacer Widget

--- lib/Basic_Layout/Rows_Columns.dart
+++ lib/Basic_Layout/Rows_Columns.dart
@@ -20,16 +20,41 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      // Learning MainAxisAlignment
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      // Learnin CrossAxisAlignment
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         BlueBox(),
         BlueBox(),
         BlueBox(),
         Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            //Learning Expanded
+            Expanded(child: BlueBox(),),
+            // Learning Flexible
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 10,
+              child: BiggerBlueBox(),
+            ),
+            Flexible(
+              fit: FlexFit.loose,
+              flex: 10,
+              child: BlueBox(),
+            ),
+            // Learning SizedBox
+            SizedBox(
+              width: 100,
+              child: BiggerBlueBox(),
+            ),
             BlueBox(),
-            BlueBox(),
+            SizedBox(width: 170),
+            BiggerBlueBox(),
+            // Learning Spacer
+            Spacer(flex: 10,),
             BlueBox(),
           ],
         ),
@@ -48,6 +73,22 @@ class BlueBox extends StatelessWidget {
       height: 50,
       decoration: BoxDecoration(
         color: Colors.purpleAccent,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+class BiggerBlueBox extends StatelessWidget {
+  const BiggerBlueBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.greenAccent,
         border: Border.all(),
       ),
     );
