58b605a900eb1490e5f30c8be77c9ad551b5f9f6
EvolViper <74658373+EvolViper@users.noreply.github.com>
2022-01-13 14:34:44 +0700

    Learning Basic Layout

---
+++ lib/Basic_Layout/Rows_Columns.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Learning Basic Layout',
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    ),
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        BlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.purpleAccent,
+        border: Border.all(),
+      ),
+    );
+  }
+}
